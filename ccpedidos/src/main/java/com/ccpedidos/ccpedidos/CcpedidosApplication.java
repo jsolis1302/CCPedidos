bf14bdf7f3d1452f0bc0172ccbdbc46cc4e31da1
Jorge Solis <solisjorge1302@gmail.com>
2025-04-18 21:07:09 -0600

    backend proyect
    added spring project

---
+++ ccpedidos/src/main/java/com/ccpedidos/ccpedidos/CcpedidosApplication.java
@@ -0,0 +1,13 @@
+package com.ccpedidos.ccpedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcpedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcpedidosApplication.class, args);
+	}
+
+}
